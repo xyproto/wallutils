d4ea33fc54f6ec2e46a7d9ee41241a7d6109aa5a
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-08 11:24:20 +0100

    Update dependencies

--- vendor/github.com/xyproto/event/eventloop.go
+++ vendor/github.com/xyproto/event/eventloop.go
@@ -17,6 +17,18 @@ func (es *Events) Add(e *Event) {
 	*es = append(*es, e)
 }
 
+// Once runs a given action only once, within a 1 second window of time
+func (es *Events) Once(when time.Time, action func()) {
+	// The window is also used as the cooldown
+	es.Add(New(when, 1*time.Second, 1*time.Second, action))
+}
+
+// OnceWindow runs a given action only once, within a custom duration
+func (es *Events) OnceWindow(when time.Time, window time.Duration, action func()) {
+	// The window is also used as the cooldown
+	es.Add(New(when, window, window, action))
+}
+
 // Loop launches an event loop that will sleep the given duration at every loop.
 func (es *Events) Loop(sleep time.Duration) {
 	// Use an endless event loop
