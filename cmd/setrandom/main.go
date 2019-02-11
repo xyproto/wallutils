a05b998f1041febbe7fddbbc7cfac687f3de196f
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-11 11:46:06 +0100

    Let setrandom take a directory as the first argument

--- cmd/setrandom/main.go
+++ cmd/setrandom/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/xyproto/monitor"
@@ -19,24 +17,33 @@ func init() {
 }
 
 func main() {
-	fmt.Println(versionString)
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Type \"yes\" if you want to change the desktop wallpaper to a random image from /usr/share/pixmaps: ")
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if len(os.Args) == 1 {
+		fmt.Println(versionString)
+		fmt.Println("Please specify a directory to choose wallpapers from.")
 		os.Exit(1)
-	}
-	if strings.TrimSpace(text) != "yes" {
-		fmt.Println("OK, made no changes.")
-		os.Exit(0)
 	}
 
-	matches, err := filepath.Glob("/usr/share/pixmaps/*.png")
+	dir := os.Args[1]
+
+	pngMatches, err := filepath.Glob(filepath.Join(dir, "/*.png"))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
+	jpgMatches, err := filepath.Glob(filepath.Join(dir, "/*.jpg"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	matches := append(pngMatches, jpgMatches...)
+
+	if len(matches) == 0 {
+		fmt.Fprintln(os.Stderr, "Found no png or jpg files in "+dir)
+		os.Exit(1)
+	}
+
 	imageFilename := matches[rand.Int()%len(matches)]
 	if absImageFilename, err := filepath.Abs(imageFilename); err == nil {
 		imageFilename = absImageFilename
