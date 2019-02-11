775bba9a73d63b14883b3591271048370018c980
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-11 14:20:22 +0100

    Add functions for prefix and suffix detection

--- utils.go
+++ utils.go
@@ -6,7 +6,13 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
+
+// c formats a timestamp as HH:MM
+func c(t time.Time) string {
+	return fmt.Sprintf("%.2d:%.2d", t.Hour(), t.Minute())
+}
 
 // has checks if a string slice has the given element
 func has(sl []string, e string) bool {
@@ -80,4 +86,66 @@ func containsE(envVar, subString string) bool {
 
 func hasE(envVar string) bool {
 	return os.Getenv(envVar) != ""
+}
+
+// CommonPrefix will find the longest common prefix in a slice of strings
+func CommonPrefix(sl []string) string {
+	if len(sl) == 0 {
+		return ""
+	}
+	shortestLength := len(sl[0])
+	shortestString := sl[0]
+	for _, s := range sl {
+		if len(s) < shortestLength {
+			shortestLength = len(s)
+			shortestString = s
+		}
+	}
+	if shortestLength == 0 {
+		return ""
+	}
+	for i := 1; i < shortestLength; i++ {
+		for _, s := range sl {
+			if !strings.HasPrefix(s, shortestString[:i]) {
+				return shortestString[:i-1]
+			}
+		}
+	}
+	return shortestString
+}
+
+// CommonPrefix will find the longest common suffix in a slice of strings
+func CommonSuffix(sl []string) string {
+	if len(sl) == 0 {
+		return ""
+	}
+	shortestLength := len(sl[0])
+	shortestString := sl[0]
+	for _, s := range sl {
+		if len(s) < shortestLength {
+			shortestLength = len(s)
+			shortestString = s
+		}
+	}
+	if shortestLength == 0 {
+		return ""
+	}
+	for i := 1; i < shortestLength; i++ {
+		for _, s := range sl {
+			if !strings.HasSuffix(s, shortestString[shortestLength-i:]) {
+				return shortestString[shortestLength-(i-1):]
+			}
+		}
+	}
+	return shortestString
+}
+
+// Meat returns the meat of the string: the part that is after the prefix and
+// before the suffix. Will return the given string if it is too short to
+// contain the prefix and suffix.
+func Meat(s, prefix, suffix string) string {
+	if len(s) < (len(prefix) + len(suffix)) {
+		return s
+	}
+	return s[len(prefix) : len(s)-len(suffix)]
 }
