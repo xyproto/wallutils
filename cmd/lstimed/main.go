baafb9ed71f971b422df526b3f0d907444e22f34
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-11 09:34:40 +0100

    Add the "-l" flag for also listing paths

--- cmd/lstimed/main.go
+++ cmd/lstimed/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/xyproto/monitor"
+	"os"
 )
 
 func main() {
+	alsoPrintPath := len(os.Args) > 1 && os.Args[1] == "-l"
 	_, gnomeWallpapers := monitor.FindWallpapers()
-	for _, gb := range gnomeWallpapers {
-		fmt.Println(gb.CollectionName)
+	for _, gw := range gnomeWallpapers {
+		if alsoPrintPath {
+			numEvents := len(gw.Config.Statics) + len(gw.Config.Transitions)
+			fmt.Printf("%s\t%s\tevents: %d\n", gw.CollectionName, gw.Path, numEvents)
+		} else {
+			fmt.Printf("%s\n", gw.CollectionName)
+		}
 	}
 }
