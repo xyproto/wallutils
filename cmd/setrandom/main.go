a88e31f10f5cd973e07cd2b49e7fb0902d967b8b
Alexander F. Rødseth <rodseth@gmail.com>
2019-03-19 10:15:51 +0100

    Add a verbose flag (-v)

--- cmd/setrandom/main.go
+++ cmd/setrandom/main.go
@@ -20,12 +20,21 @@ func main() {
 		os.Exit(0)
 	}
 
-	if len(os.Args) == 1 {
-		fmt.Println("Please specify a directory to choose wallpapers from.")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Please specify a directory to choose wallpapers from.")
 		os.Exit(1)
 	}
-
 	dir := os.Args[1]
+
+	verbose := false
+	if len(os.Args) > 1 && os.Args[1] == "-v" {
+		verbose = true
+		if len(os.Args) < 3 {
+			fmt.Fprintln(os.Stderr, "Please specify a directory to choose wallpapers from.")
+			os.Exit(1)
+		}
+		dir = os.Args[2]
+	}
 
 	pngMatches, err := filepath.Glob(filepath.Join(dir, "/*.png"))
 	if err != nil {
@@ -50,12 +59,9 @@ func main() {
 	if absImageFilename, err := filepath.Abs(imageFilename); err == nil {
 		imageFilename = absImageFilename
 	}
-	if _, err := os.Stat(imageFilename); os.IsNotExist(err) {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+
 	fmt.Println("Setting background image to: " + imageFilename)
-	if err := wallutils.SetWallpaper(imageFilename); err != nil {
+	if err := wallutils.SetWallpaperVerbose(imageFilename, verbose); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
