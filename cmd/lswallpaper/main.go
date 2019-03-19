0b830840d02258a361aa04b21ba9929a3435fbcb
Alexander F. Rødseth <rodseth@gmail.com>
2019-03-19 14:50:48 +0100

    Better argument handling

--- cmd/lswallpaper/main.go
+++ cmd/lswallpaper/main.go
@@ -2,26 +2,65 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"github.com/xyproto/wallutils"
 	"os"
 )
 
-func main() {
-	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Println(wallutils.VersionString)
-		os.Exit(0)
-	}
+func listWallpapersAction(c *cli.Context) error {
 	searchResults, err := wallutils.FindWallpapers()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+		return err
 	}
-	withDetails := len(os.Args) > 1 && os.Args[1] == "-l"
+	withDetails := c.IsSet("long")
+	collectionMarker := c.IsSet("star")
 	for _, wp := range searchResults.Wallpapers() {
-		if withDetails {
+		if withDetails && collectionMarker {
 			fmt.Println(wp)
+		} else if withDetails {
+			fmt.Printf("%dx%d\t%16s\t%s\n",
+				wp.Width, wp.Height, wp.CollectionName, wp.Path)
+		} else if collectionMarker {
+			star := " "
+			if wp.PartOfCollection {
+				star = "*"
+			}
+			fmt.Printf("(%s) %s\n", star, wp.Path)
 		} else {
 			fmt.Println(wp.Path)
 		}
+	}
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "lswallpaper"
+	app.Usage = "list all wallpapers on the system"
+	app.UsageText = "lswallpaper [options]"
+
+	app.Version = wallutils.VersionString
+	app.HideHelp = true
+
+	cli.VersionFlag = cli.BoolFlag{
+		Name:  "version, V",
+		Usage: "output version information",
+	}
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "long, l",
+			Usage: "also list paths",
+		},
+		cli.BoolFlag{
+			Name:  "star, s",
+			Usage: "prefix wallpapers with a star if they are part of a collection",
+		},
+	}
+
+	app.Action = listWallpapersAction
+	if err := app.Run(os.Args); err != nil {
+		wallutils.Quit(err)
 	}
 }
