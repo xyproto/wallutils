da0d4a4c4de7cf9d4617dcfb868846e15cf16a22
Alexander F. Rødseth <rodseth@gmail.com>
2019-03-20 23:19:43 +0100

    Update test

--- monconf_test.go
+++ monconf_test.go
@@ -1,19 +1,14 @@
 package wallutils
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestParseMonitorConfiguration(t *testing.T) {
-	mc, err := ParseMonitors()
+	mc, err := NewMonitorConfiguration()
 	if err != nil {
-		panic(err)
+		// Ignore this test if ~/.config/monitor.xml does not exist
+		return
 	}
-	for _, conf := range mc.Configurations {
-		for _, output := range conf.Outputs {
-			fmt.Println("OUTPUT", output)
-		}
-	}
-	fmt.Println("NO OUTPUT")
+	mc.Overlapping()
 }
