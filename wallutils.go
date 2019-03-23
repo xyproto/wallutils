22fc7625eabdb1c3ee004c855d3c81ed267d45ec
Alexander F. Rødseth <rodseth@gmail.com>
2019-03-23 09:45:57 +0100

    Refactoring

--- wallutils.go
+++ wallutils.go
@@ -1,21 +1,5 @@
 // Package wallutils can deal with monitors, resolution, dpi, wallpapers, wallpaper collections, timed wallpapers and converting GNOME timed wallpapers to the Simple Timed Wallpaper format.
 package wallutils
 
-import (
-	"fmt"
-	"os"
-	"strings"
-)
-
 // VersionString is the current version of wallutils and all included utilities
 const VersionString = "5.3.5"
-
-// Quit with a nicely formatted error message to stderr
-func Quit(err error) {
-	msg := err.Error()
-	if !strings.HasSuffix(msg, ".") && !strings.HasSuffix(msg, "!") && !strings.Contains(msg, ":") {
-		msg += "."
-	}
-	fmt.Fprintf(os.Stderr, "%s%s\n", strings.ToUpper(string(msg[0])), msg[1:])
-	os.Exit(1)
-}
