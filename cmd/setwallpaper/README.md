6c0d86a408fa30cc773f47ffcb65527bc1c3dd88
Alexander F. Rødseth <rodseth@gmail.com>
2019-03-28 12:50:28 +0100

    Support wallpaper modes and image URLs

--- cmd/setwallpaper/README.md
+++ cmd/setwallpaper/README.md
@@ -1,3 +1,39 @@
 `setwallpaper` aims to be able to change the desktop wallpaper, for any windowmanager.
 
 The first argument is the image to use.
+
+URLs are supported.
+
+# Wallpaper Modes
+
+## Sway
+
+* `stretch`
+* `fill`
+* `fit`
+* `center`
+* `tile`
+
+## Feh
+
+* `fill`
+* `center`
+* `max`
+* `scale`
+* `tile`
+
+## Gnome3 / Cinnamon / MATE / Deepin
+
+* `none`
+* `wallpaper`
+* `centered`
+* `scaled`
+* `stretched`
+* `zoom`
+* `spanned`
+
+For compatibility:
+* `fill` is an alias for `stretched`
+* `center` is an alias for `centered`
+* `scale` is an alias for `scaled`
+* `tile` is an alias for `wallpaper`
--- cmd/setwallpaper/main.go
+++ cmd/setwallpaper/main.go
@@ -3,18 +3,72 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/urfave/cli"
 	"github.com/xyproto/wallutils"
 )
 
+// exists checks if the given path exists
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+// download can download a file to the given filename
+// Use force if existing files should be overwritten.
+func download(url, filename string, force bool) error {
+	// Prepare the client
+	var client http.Client
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// Download the file
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	// Check if the file exists (and that force is not enabled)
+	if exists(filename) && !force {
+		return fmt.Errorf("%s already exists", filename)
+	}
+	// Write the file
+	return ioutil.WriteFile(filename, b, 0644)
+}
+
 func setWallpaperAction(c *cli.Context) error {
 	if c.NArg() == 0 {
-		return errors.New("please specify an image filename")
+		return errors.New("please specify an image filename or URL")
 	}
 	imageFilename := c.Args().Get(0)
+
+	// Retrieve flags from the context
+	verbose := c.IsSet("verbose")
+	mode := c.String("mode")
+
+	// Check if the argument is an URL that uses the http or https protocol
+	if strings.HasPrefix(imageFilename, "http://") || strings.HasPrefix(imageFilename, "https://") {
+		u, err := url.Parse(imageFilename)
+		if err == nil { // no error
+			// TODO: Use a function for getting the temp directory
+			downloadFilename := filepath.Join("/tmp/", filepath.Base(imageFilename))
+			if verbose {
+				fmt.Println("Downloading " + u.String())
+			}
+			if err := download(u.String(), downloadFilename, true); err != nil {
+				return err
+			}
+			// Use the downloaded image
+			imageFilename = downloadFilename
+		}
+	}
 
 	// Find the absolute path
 	absImageFilename, err := filepath.Abs(imageFilename)
@@ -22,11 +76,8 @@ func setWallpaperAction(c *cli.Context) error {
 		imageFilename = absImageFilename
 	}
 
-	// Check if the verbose flag is set
-	verbose := c.IsSet("verbose")
-
 	// Set the desktop wallpaper
-	if err := wallutils.SetWallpaperVerbose(imageFilename, verbose); err != nil {
+	if err := wallutils.SetWallpaperCustom(imageFilename, verbose, mode); err != nil {
 		return fmt.Errorf("could not set wallpaper: %s", err)
 	}
 	return nil
@@ -36,8 +87,8 @@ func main() {
 	app := cli.NewApp()
 
 	app.Name = "setwallpaper"
-	app.Usage = "change the desktop wallpaper"
-	app.UsageText = "setwallpaper [options] [path to JPEG or PNG image]"
+	app.Usage = "set the desktop wallpaper"
+	app.UsageText = "setwallpaper [options] [path or URL to JPEG or PNG image]"
 
 	app.Version = wallutils.VersionString
 	app.HideHelp = true
@@ -51,6 +102,11 @@ func main() {
 		cli.BoolFlag{
 			Name:  "verbose, v",
 			Usage: "verbose output",
+		},
+		cli.StringFlag{
+			Name:  "mode",
+			Value: "fill",
+			Usage: "wallpaper mode (fill | center | scale | tile) + modes specific to the currently running DE/WM",
 		},
 	}
 
