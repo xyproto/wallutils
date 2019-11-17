fcd768d197227ae47d6fc4b0e8bec541f2ee9610
Alexander F. Rødseth <rodseth@gmail.com>
2019-11-17 16:30:34 +0100

    Add a verbose output argument to the X11-related C code

--- xwallpaper.h
+++ xwallpaper.h
@@ -8,19 +8,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 static Display* dpy;
 static int screen;
 static Window root;
 
 // Read black/white X bitmap file
-static Pixmap ReadXBMFile(char* filename, unsigned int* width, unsigned int* height, int* x_hot, int* y_hot)
+static Pixmap ReadXBMFile(char* filename, unsigned int* width, unsigned int* height, int* x_hot, int* y_hot, bool verbose)
 {
     Pixmap bitmap;
     int status = XReadBitmapFile(dpy, root, filename, width, height, &bitmap, x_hot, y_hot);
     if (status == BitmapSuccess) {
         return (bitmap);
-    } else if (status == BitmapOpenFailed) {
+    }
+    if (status == BitmapOpenFailed) {
         fprintf(stderr, "can't open file: %s\n", filename);
     } else if (status == BitmapFileInvalid) {
         fprintf(stderr, "bad bitmap format file: %s\n", filename);
@@ -30,7 +32,7 @@ static Pixmap ReadXBMFile(char* filename, unsigned int* width, unsigned int* hei
 }
 
 // Read colorful X pixmap file
-static Pixmap ReadXPMFile(char* filename, unsigned int* width, unsigned int* height)
+static Pixmap ReadXPMFile(char* filename, unsigned int* width, unsigned int* height, bool verbose)
 {
     Pixmap pixmap;
     XpmAttributes attributes;
@@ -46,36 +48,65 @@ static Pixmap ReadXPMFile(char* filename, unsigned int* width, unsigned int* hei
 }
 
 // Set the background image to the given pixmap
-static void SetBackgroundToPixmapAndFree(Pixmap bitmap, unsigned int width, unsigned int height)
+static void SetBackgroundToPixmapAndFree(Pixmap bitmap, unsigned int width, unsigned int height, bool verbose)
 {
     XGCValues gc_init;
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XCreateGC\n");
     GC gc = XCreateGC(dpy, root, GCForeground | GCBackground, &gc_init);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XCreatePixmap\n");
     Pixmap pix = XCreatePixmap(dpy, root, width, height, (unsigned int)DefaultDepth(dpy, screen));
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XCopyPlane\n");
     XCopyPlane(dpy, bitmap, pix, gc, 0, 0, width, height, 0, 0, (unsigned long)1);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XSetWindowBackgroundPixmap\n");
     XSetWindowBackgroundPixmap(dpy, root, pix);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XFreeGC\n");
     XFreeGC(dpy, gc);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XFreePixmap(dpy, bitmap)\n");
     XFreePixmap(dpy, bitmap);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XFreePixmap(dpy, pix)\n");
     XFreePixmap(dpy, pix);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree: XClearWindow\n");
     XClearWindow(dpy, root);
 }
 
-int SetBackground(char* filename)
+int SetBackground(char* filename, bool verbose)
 {
     unsigned int ww;
     unsigned int hh;
 
+    if (verbose) printf("X11: SetBackground, filename = %s\n", filename);
+
+    if (verbose) printf("X11: XOpenDisplay... ");
     dpy = XOpenDisplay("");
     if (!dpy) {
+        if (verbose) printf("failed\n");
         return -1;
     }
+    if (verbose) printf("ok\n");
+
     screen = DefaultScreen(dpy);
     root = RootWindow(dpy, screen);
 
-    //Pixmap bitmap = ReadBitmapFile(filename, &ww, &hh, (int*)NULL, (int*)NULL);
-    Pixmap bitmap = ReadXPMFile(filename, &ww, &hh);
-    SetBackgroundToPixmapAndFree(bitmap, ww, hh);
+    if (verbose) printf("X11: root window OK\n");
+
+    //Pixmap bitmap = ReadBitmapFile(filename, &ww, &hh, (int*)NULL, (int*)NULL, verbose);
+
+    if (verbose) printf("X11: ReadXPMFile\n");
+    Pixmap bitmap = ReadXPMFile(filename, &ww, &hh, verbose);
+
+    if (verbose) printf("X11: SetBackgroundToPixmapAndFree\n");
+    SetBackgroundToPixmapAndFree(bitmap, ww, hh, verbose);
 
     if (dpy) {
+        if (verbose)  printf("X11: XCloseDisplay\n");
         XCloseDisplay(dpy);
     }
 
