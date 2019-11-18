3648784274addca2c2724f0976669f8b584a9160
Alexander F. Rødseth <rodseth@gmail.com>
2019-11-18 12:36:15 +0100

    Update dependencies

--- vendor/github.com/xyproto/xbg/README.md
+++ vendor/github.com/xyproto/xbg/README.md
@@ -18,7 +18,7 @@ A test-utility is included in `cmd/grumpybg/`.
 
 ## General info
 
-* Version: 0.0.4
+* Version: 0.0.5
 * License: MIT
 * Author: Alexander F. Rødseth &lt;xyproto@archlinux.org&gt;
 
---
+++ vendor/github.com/xyproto/xbg/easy.go
@@ -0,0 +1,42 @@
+package xbg
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// SetWallpaper will set the given image filename as the wallpaper,
+// The mode string can be:
+//   "center" for "center mode"
+//   "zoom", "zoomed", "fill" or "max" for "zoom mode"
+//   "scale", "scaled", "stretch" or "streatched" for "scale mode"
+// If verbose is set, some output will be written to stdout.
+func SetWallpaper(imageFilename, mode string, verbose bool) error {
+	// This check is redundant, but it's nice to check it before checking if the WM is ready
+	if !exists(imageFilename) {
+		return fmt.Errorf("no such file: %s", imageFilename)
+	}
+	wm := X11{}
+	if !(wm.Running() && wm.ExecutablesExists()) {
+		return errors.New("found no working method for setting the desktop wallpaper, maybe X11 is not installed or DISPLAY not set")
+	}
+	if verbose {
+		fmt.Printf("Using the %s backend.\n", wm.Name())
+	}
+	wm.SetVerbose(verbose)
+	wm.SetMode(mode)
+	if err := wm.SetWallpaper(imageFilename); err != nil {
+		if verbose {
+			fmt.Fprintf(os.Stderr, "failed: %v\n", err)
+		}
+		// If the wallpaper mode is wrong, don't try the next backend, but return the error
+		if strings.Contains(err.Error(), "invalid desktop wallpaper mode") {
+			return err
+		}
+		// This did not work out
+		return errors.New("could not use the X11 backend")
+	}
+	return nil
+}
