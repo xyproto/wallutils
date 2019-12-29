86e7051fb209195425f5474c2993e6fdf11d8c6a
Alexander F. Rødseth <rodseth@gmail.com>
2019-12-29 19:26:39 +0100

    Add support for modes and recursive searching for setrandom
    The "-r" or "--recursive" flag resolves #18
    The "-m" or "--mode" flag resolves #19

--- cmd/setrandom/main.go
+++ cmd/setrandom/main.go
@@ -20,29 +20,55 @@ func setRandomWallpaperAction(c *cli.Context) error {
 	if c.NArg() == 0 {
 		return errors.New("please specify a directory to choose wallpapers from")
 	}
-	dir := c.Args().Get(0)
+	var (
+		dir = c.Args().Get(0)
 
-	// Check if the verbose flag is set
-	verbose := c.IsSet("verbose")
+		// Retrieve flags from the context
+		verbose   = c.IsSet("verbose")
+		recursive = c.IsSet("recursive")
+		mode      = c.String("mode")
+		onlyLarge = c.IsSet("onlylarge")
 
-	pngMatches, err := filepath.Glob(filepath.Join(dir, "/*.png"))
-	if err != nil {
-		return err
+		// Prepare to search for images
+		matches []string
+		err     error
+	)
+
+	if recursive {
+
+		if verbose {
+			fmt.Printf("Searching %s recursively: ", dir)
+		}
+		// onlyLarge means >= 640x480
+		matches, err = wallutils.FindImagesAt(dir, []string{".png", ".jpg", ".jpeg"}, onlyLarge)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			return err
+		}
+		if verbose {
+			fmt.Printf("found %d images\n", len(matches))
+		}
+
+	} else {
+
+		pngMatches, err := filepath.Glob(filepath.Join(dir, "/*.png"))
+		if err != nil {
+			return err
+		}
+		matches = append(matches, pngMatches...)
+
+		jpgMatches, err := filepath.Glob(filepath.Join(dir, "/*.jpg"))
+		if err != nil {
+			return err
+		}
+		matches = append(matches, jpgMatches...)
+
+		jpegMatches, err := filepath.Glob(filepath.Join(dir, "/*.jpeg"))
+		if err != nil {
+			return err
+		}
+		matches = append(matches, jpegMatches...)
 	}
-
-	jpgMatches, err := filepath.Glob(filepath.Join(dir, "/*.jpg"))
-	if err != nil {
-		return err
-	}
-
-	jpegMatches, err := filepath.Glob(filepath.Join(dir, "/*.jpeg"))
-	if err != nil {
-		return err
-	}
-
-	matches := pngMatches
-	matches = append(matches, jpgMatches...)
-	matches = append(matches, jpegMatches...)
 
 	if len(matches) == 0 {
 		return fmt.Errorf("found no .png, .jpg or .jpeg files in %s", dir)
@@ -56,7 +82,7 @@ func setRandomWallpaperAction(c *cli.Context) error {
 	if verbose {
 		fmt.Printf("Setting background image to: %s\n", imageFilename)
 	}
-	return wallutils.SetWallpaperVerbose(imageFilename, verbose)
+	return wallutils.SetWallpaperCustom(imageFilename, mode, verbose)
 }
 
 func main() {
@@ -78,6 +104,19 @@ func main() {
 		cli.BoolFlag{
 			Name:  "verbose, v",
 			Usage: "verbose output",
+		},
+		cli.BoolFlag{
+			Name:  "recursive, r",
+			Usage: "search recursively",
+		},
+		cli.BoolFlag{
+			Name:  "onlylarge, l",
+			Usage: "only images >= 640x480 pixels",
+		},
+		cli.StringFlag{
+			Name:  "mode, m",
+			Value: "stretch", // the default value
+			Usage: "wallpaper mode (stretch | center | tile | scale) \n\t+ modes specific to the currently running DE/WM",
 		},
 	}
 
